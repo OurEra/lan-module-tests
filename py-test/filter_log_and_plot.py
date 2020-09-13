9b475748b7a7941d7b1bce11bb9910d6528435f9
shiruiwei <shiruiwei@qiniu.com>
2020-09-13 12:39:39 +0800

    Update python json filter test

--- py-test/filter_log_and_plot.py
+++ py-test/filter_log_and_plot.py
@@ -2,19 +2,87 @@ import json
 from datetime import datetime
 from termcolor import colored
 
-orignal_log = open("test-json.txt", "r", encoding='utf-8')
+orignal_log = open("test-json2.txt", "r", encoding='utf-8')
+
+filter_out_media = True
+
+
+# IDLE,
+# CONNECTING,
+# CONNECTED,
+# RECONNECTING,
+# RECONNECTED,
+def convert_room_name(id):
+    if id == 0:
+        return 'IDLE'
+    elif id == 1:
+        return 'CONNECTING'
+    elif id == 2:
+        return 'CONNECTED'
+    elif id == 3:
+        return 'RECONNECTING'
+    elif id == 4:
+        return 'RECONNECTED'
+
+# NEW,
+# CHECKING,
+# CONNECTED,
+# COMPLETED,
+# FAILED,
+# DISCONNECTED,
+# CLOSED;
+def convert_ice_state(id, type):
+    pc = 'PUB'
+    if type == 0:
+        pc = 'PUB'
+    elif type == 1:
+        pc = 'SUB'
+
+    if id == 0:
+        return pc + ' NEW'
+    elif id == 1:
+        return pc + ' CHECKING'
+    elif id == 2:
+        return pc + ' CONNECTED'
+    elif id == 3:
+        return pc + ' COMPLETED'
+    elif id == 4:
+        return pc + ' FAILED'
+    elif id == 5:
+        return pc + ' DISCONNECTED'
+    elif id == 6:
+        return pc + ' CLOSED'
 
 lines = []
 for line in orignal_log:
     json_content = json.loads(line)
-    if 'item' in json_content:
+    try:
+        checkitem = json_content['item']['timestamp']
         lines.append(json_content)
-    print (json.dumps(json_content, indent=4))
+    except KeyError:
+        pass
+    #print (json.dumps(json_content, indent=4))
 
 # https://stackoverflow.com/questions/26924812/python-sort-list-of-json-by-value
-lines = sorted(lines, key=lambda k: k['item']['timestamp'])
+lines = sorted(lines, key=lambda k: int(k['item']['timestamp']))
 for line in lines:
-    print (colored(line['item']['event_name'] + " : " + datetime.utcfromtimestamp(line['item']['timestamp'] / 1000).strftime('%Y-%m-%d %H:%M:%S'), 'red'))
-    print (line)
+    event = line['item']['event_name']
+    rdate = datetime.fromtimestamp(line['item']['timestamp'] / 1000).strftime('%Y-%m-%d %H:%M:%S')
+    platform = line['base']['os_platform']
+    if line['item']['event_id'] == 15:
+        ice = convert_ice_state(line['item']['state'], line['item']['pc_type'])
+        print (colored(rdate + " : " + platform + " : " + event + " : " + ice, 'red'))
+    elif line['item']['event_id'] == 18:
+        room = convert_room_name(line['item']['room_state'])
+        print (colored(rdate + " : " + platform + " : " + event + " : " + room, 'red'))
+    elif filter_out_media and line['item']['event_id'] == 30:
+        pass
+    else:
+        print (colored(rdate + " : " + platform + " : " + event, 'red'))
+
+    if filter_out_media and line['item']['event_id'] == 30:
+        pass
+    else:
+        print (line)
 
 orignal_log.close()
