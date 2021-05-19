8a6c93fb4969d943cffce35fc81cf3e534ed312e
shiruiwei <shiruiwei@qiniu.com>
2021-05-19 16:04:16 +0800

    Add kotlin test and gtest

--- cpp-test/build.sh
+++ cpp-test/build.sh
@@ -1,4 +1,4 @@
-clang -std=c++17 -lstdc++ -I../external/glm main.cpp \
+clang -std=c++17 -lstdc++ -lpthread -I../external/glm main.cpp \
         TestScale.cpp TestVector.cpp TestSet.cpp TestSlot.cpp TestDeque.cpp TestGLM.cpp \
         TestTimestampExtrapolator.cpp TestJMP.cpp TestMap.cpp TestPtr.cpp \
         -o test-cpp
--- external/state_thread_example_client.c
+++ external/state_thread_example_client.c
@@ -1,16 +1,16 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<sys/wait.h>
-#include<sys/stat.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
-#include<netdb.h>
-#include<unistd.h>
-#include<errno.h>
-#include<signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
 
 #define RET_OK 0
 #define RET_ERR -1
@@ -23,67 +23,60 @@
 
 int main(int argc,char **argv)
 {
-	int sockfd,ret=RET_OK;
+	int sockfd, ret = RET_OK;
 	struct sockaddr_in servaddr;
 	struct hostent *server;
 
-	char buffer[BUFFER_SIZE];//字符串指针
+	char buffer[BUFFER_SIZE];
 
-	if(argc<2)
-	{
-		fprintf(stderr,"usage %s hostname\n",argv[0]);
+	if(argc < 2) {
+		fprintf(stderr,"usage %s hostname\n", argv[0]);
 		return RET_ERR;
 	}
-	if((server=gethostbyname(argv[1]))==NULL) //由域名获取IP地址
-	{
+	if((server = gethostbyname(argv[1])) == NULL) {
 		herror("gethostbyname.");
 		return RET_ERR;
 	}
-	if((sockfd=socket(AF_INET,SOCK_STREAM,0))<0) //创建套接字
-	{
+
+	if((sockfd = socket(AF_INET,SOCK_STREAM, 0)) < 0) {
 		perror("ERROR opening socket");
 		return RET_ERR;
 	}
 
-	memset(&servaddr,0,sizeof(servaddr));//申请信息表空间
+	memset(&servaddr, 0, sizeof(servaddr));
 
-	servaddr.sin_family=AF_INET;//设为IPv4
-	servaddr.sin_addr.s_addr=*(uint32_t *)server->h_addr;//地址转换,32位大端字序
-	servaddr.sin_port=htons((uint16_t)ECHO_PORT);//设定端口,16位大端字序转换
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = *(uint32_t *)server->h_addr;
+	servaddr.sin_port = htons((uint16_t)ECHO_PORT);
 
-	if((ret=connect(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr)))<0) //创建连接
-	{
+	if((ret = connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr))) < 0) {
 		perror("ERROR connecting");
 		goto failed;
 	}
-	while(1)
-	{
+
+	while(1) {
 		printf("Enter the message : ");
-		if(fgets(buffer,sizeof(buffer)-1,stdin)==NULL) //输入字符到buffer指针
-		{
+		if(fgets(buffer, sizeof(buffer)-1, stdin) == NULL) {
 			break;
 		}
-		if((ret=write(sockfd,buffer,strlen(buffer)))<0)	//把buffer的内容 写入strlen(buffer)的字节到sockfd的socket中
-		{
+		if((ret = write(sockfd, buffer, strlen(buffer))) < 0) {
 			perror("ERROR write to socket");
 			break;
 		}
-		if((ret=read(sockfd,buffer,sizeof(buffer)-1))<0)//读取到buffer中
-		{
+		if((ret = read(sockfd, buffer, sizeof(buffer)-1)) < 0) {
 			perror("ERROR read from socket");
 			break;
 		}
-		if(ret==0)
-		{
+		if(ret == 0) {
 			printf("Server disconnect\n");
 			break;
 		}
-		buffer[ret]=0;	//
-		printf("Server echo message: %s\n",buffer);
+		buffer[ret] = 0;
+		printf("Server echo message: %s\n", buffer);
 	}
 	failed:
 		close(sockfd);
-		return ret<0?RET_ERR:RET_OK;
+		return ret < 0 ? RET_ERR:RET_OK;
 }
 
 
