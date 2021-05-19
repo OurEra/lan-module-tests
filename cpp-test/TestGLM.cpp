3c5fa1e6f3936ec4c5cbc4cea7d75bf6fa6c187e
shiruiwei <shiruiwei@qiniu.com>
2021-05-19 11:53:19 +0800

    Move glm to sub module

--- cpp-test/TestGLM.cpp
+++ cpp-test/TestGLM.cpp
@@ -1,12 +1,12 @@
 #include "TestGLM.h"
-#include "vendor/glm/vec3.hpp" // glm::vec3
-#include "vendor/glm/vec4.hpp" // glm::vec4
-#include "vendor/glm/mat4x4.hpp" // glm::mat4
-#include "vendor/glm/ext/matrix_transform.hpp" // glm::translate, glm::rotate, glm::scale
-#include "vendor/glm/ext/matrix_clip_space.hpp" // glm::perspective
-#include "vendor/glm/ext/scalar_constants.hpp" // glm::pi
-#include "vendor/glm/gtx/string_cast.hpp"
-#include "vendor/glm/ext/matrix_transform.hpp"
+#include "glm/vec3.hpp" // glm::vec3
+#include "glm/vec4.hpp" // glm::vec4
+#include "glm/mat4x4.hpp" // glm::mat4
+#include "glm/ext/matrix_transform.hpp" // glm::translate, glm::rotate, glm::scale
+#include "glm/ext/matrix_clip_space.hpp" // glm::perspective
+#include "glm/ext/scalar_constants.hpp" // glm::pi
+#include "glm/gtx/string_cast.hpp"
+#include "glm/ext/matrix_transform.hpp"
 #include <stdio.h>
 
 void TestGLM::setUp() {
--- cpp-test/build.sh
+++ cpp-test/build.sh
@@ -1,4 +1,4 @@
-clang -std=c++17 -lstdc++ main.cpp \
+clang -std=c++17 -lstdc++ -I../external/glm main.cpp \
         TestScale.cpp TestVector.cpp TestSet.cpp TestSlot.cpp TestDeque.cpp TestGLM.cpp \
         TestTimestampExtrapolator.cpp TestJMP.cpp TestMap.cpp TestPtr.cpp \
         -o test-cpp
